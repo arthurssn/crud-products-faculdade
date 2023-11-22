4334eb6b01a7f87dc9d5889e379ab71e1955d698
Arthur Novaes <arthurnovaes55@gmail.com>
2023-11-22 11:51:40 -0300

    feat: implementa findbyid

--- src/main/java/com/crud/productscrud/controllers/CRUDController.java
+++ src/main/java/com/crud/productscrud/controllers/CRUDController.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
 
 public abstract class CRUDController<T> {
     private final JpaRepository<T, Long> service;
@@ -19,5 +21,13 @@ public abstract class CRUDController<T> {
     public ResponseEntity<List<T>> getAll() {
         List<T> items = service.findAll();
         return new ResponseEntity<>(items, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<T> getById(@PathVariable Long id) {
+        Optional<T> item = service.findById(id);
+        return item
+                .map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 }
